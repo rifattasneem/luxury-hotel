2eccfed4533a6f974cd294d981aba6ed43cb0688
Rifat Tasneem <77124499+rifattasneem@users.noreply.github.com>
2021-07-05 17:41:18 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	// datepicker
+	$( "#datepicker1" ).datepicker({
+		uiLibrary: 'bootstrap'
+	});
+	$( "#datepicker2" ).datepicker({
+		uiLibrary: 'bootstrap'
+	});
+
+	// dropDown
+    $(".controlgroup").controlgroup();
+});
+
+// wow
+new WOW().init();
+
