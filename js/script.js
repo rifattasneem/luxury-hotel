f9ab96a435990cf243626a33d28ce6bf978c931e
Rifat Tasneem <77124499+rifattasneem@users.noreply.github.com>
2021-07-07 05:39:05 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
 
-	// datepicker
-	$( "#datepicker1" ).datepicker({
-		uiLibrary: 'bootstrap'
-	});
-	$( "#datepicker2" ).datepicker({
-		uiLibrary: 'bootstrap'
-	});
-
 	// dropDown
     $(".controlgroup").controlgroup();
 });
 
 // wow
 new WOW().init();
+
+// datePicker
+const myDatePicker1 = MCDatepicker.create({ 
+      el: '#datepicker1',
+      bodyType: 'inline',
+      autoClose: true,
+})
+const myDatePicker2 = MCDatepicker.create({ 
+      el: '#datepicker2',
+      bodyType: 'inline',
+      autoClose: true,
+})
 
